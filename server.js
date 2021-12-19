6d9f1cb4ca1f0e53df4b53fa77b8a8256cf98e8c
Fishdestroyer <jeffwhitner@gmail.com>
2021-12-18 22:58:07 -0600

    added join statements

--- server.js
+++ server.js
@@ -17,7 +17,12 @@ const db = mysql.createConnection(
 );
 // to get all candidates
 app.get('/api/candidates', (req, res) => {
-    const sql = `SELECT * FROM candidates`;
+   // const sql = `SELECT * FROM candidates`;
+   const sql = `SELECT candidates.*, parties.name
+   AS party_name
+   FROM candidates
+   LEFT JOIN parties
+   ON candidates.party_id =parties.id`;
 
     db.query(sql, (err, rows) => {
         if (err) {
@@ -32,7 +37,13 @@ app.get('/api/candidates', (req, res) => {
 });
 //to get a single candidate
 app.get('/api/candidate/:id', (req, res) => {
-    const sql = `SELECT * FROM candidates WHERE id = ?`;
+   // const sql = `SELECT * FROM candidates WHERE id = ?`;
+   const sql = `SELECT candidates.*, parties.name
+   AS party_name
+   FROM candidates
+   LEFT JOIN parties
+   ON candidates.party_id = parties.id
+   WHERE candidates.id =?`;
     const params = [req.params.id];
 
     db.query(sql, params, (err, rows) => {
